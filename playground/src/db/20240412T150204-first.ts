ad9a5612343b64caf9634c3e2eb6973f354b53b8
tbosmans <tom.94.bosmans@gmail.com>
2024-04-13 15:50:45 +0200

    Add playground.

---
+++ playground/src/db/20240412T150204-first.ts
@@ -0,0 +1,16 @@
+import { Kysely, sql } from "kysely"
+
+export async function up(db: Kysely<unknown>): Promise<void> {
+  await db.schema
+    .createTable("users")
+    .addColumn("id", "uuid", (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn("first_name", "varchar", (col) => col.notNull())
+    .addColumn("last_name", "varchar", (col) => col.notNull())
+    .addColumn("email", "varchar", (col) => col.notNull().unique())
+    .addColumn("password", "varchar", (col) => col.notNull())
+    .execute()
+}
+
+export async function down(db: Kysely<unknown>): Promise<void> {
+  await db.schema.dropTable("users").execute()
+}
